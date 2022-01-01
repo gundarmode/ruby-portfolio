3d0969f49c5447e1f60e86cbaf88cd807f6b7d15
gundarmode <jin_masenko@hotmail.com>
2022-01-01 20:43:27 +0000

    changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -62,7 +62,7 @@ const Navigation = ({navAnimation, setNavAnimation, windowWidth, windowHeight}) 
 	// }
 
 	useEffect(() => {
-		setDisplay(location !== "/ruby-portfolio" ? true : false)
+		setDisplay(location !== "/ruby-portfolio/" ? true : false)
 		switch (location) {
 			// case "/" : 
 			// setNavAnimation(false)
